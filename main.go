fbc1d4f8c440bafd88fe65dd175ff005c296a93d
Ben Chapman <bchapman@pivotal.io>
2018-07-16 15:05:44 +0100

    Add autodetection for Azure

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
+	"net/url"
 	"os"
 	"strings"
 
@@ -14,9 +16,10 @@ import (
 
 func main() {
 	r := mux.NewRouter()
+	r.Handle("/bootstrap.min.css", http.FileServer(http.Dir("./static/")))
 	r.HandleFunc("/{iaas}", handleRequest)
 	r.HandleFunc("/{iaas}/{version}", handleRequest)
-	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
+	r.Handle("/", http.FileServer(http.Dir("./static/")))
 
 	err := http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("PORT")), r)
 	if err != nil {
@@ -39,18 +42,19 @@ func handleRequest(w http.ResponseWriter, r *http.Request) {
 		version = fmt.Sprintf("?v=%s", versionString)
 	}
 
-	switch iaasString {
-	case "auto":
+	if iaasString == "auto" {
 		xff := r.Header.Get("X-Forwarded-For")
 		splitXff := strings.Split(xff, ", ")
-		fmt.Printf("%#v\n", splitXff)
 		source, err := autodetectSource(net.ParseIP(splitXff[0]))
 		if err != nil || source == "" {
 			w.WriteHeader(http.StatusNotFound)
 			w.Write([]byte("could not autodetect IaaS"))
 			return
 		}
-		iaas = source
+		iaasString = source
+	}
+
+	switch iaasString {
 	case "aws", "amazon":
 		iaas = "aws-xen-hvm"
 	case "azure":
@@ -92,6 +96,14 @@ func autodetectSource(ipAddress net.IP) (string, error) {
 		return "aws", nil
 	}
 
+	azure, err := isAzureAddress(ipAddress)
+	if err != nil {
+		return "", err
+	}
+	if azure {
+		return "azure", nil
+	}
+
 	return "", nil
 }
 
@@ -107,8 +119,20 @@ func isGCPAddress(ipAddress net.IP) (bool, error) {
 func isAWSAddress(ipAddress net.IP) (bool, error) {
 	names, err := net.LookupAddr(ipAddress.String())
 	if err != nil {
-		return false, err
+		// will return an error if the address is not found
+		return false, nil
 	}
 
 	return strings.Contains(names[0], "amazonaws.com."), nil
+}
+
+func isAzureAddress(ipAddress net.IP) (bool, error) {
+	r, err := http.Get(fmt.Sprintf("http://www.azurespeed.com/api/region?ipOrUrl=%s", url.QueryEscape(ipAddress.String())))
+	if err != nil {
+		return false, err
+	}
+
+	jsonoutput := map[string]interface{}{}
+	json.NewDecoder(r.Body).Decode(&jsonoutput)
+	return jsonoutput["cloud"].(string) == "Azure", nil
 }
